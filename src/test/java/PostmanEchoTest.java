3695ce66a2c6bd13560e83e536513cac92ccc33b
Perepadin <82140378+Perepadin@users.noreply.github.com>
2021-07-25 11:02:44 +0300

    add test with fail

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("test user data v1"))
+                .body("data", equalTo("test user data v2"))
         ;
     }
 }
